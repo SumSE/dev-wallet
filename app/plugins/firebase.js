2ba29a800f7cb8c976e0a3ba26d32a933e1cfb2c
y-nishina <y.nishina0509@gmail.com>
2019-09-23 22:14:31 +0900

    不要なコメントを削除

--- app/plugins/firebase.js
+++ app/plugins/firebase.js
@@ -2,17 +2,6 @@ import firebase from 'firebase'
 
 if (!firebase.apps.length) {
     firebase.initializeApp({
-        
-        /*
-        apiKey: "AIzaSyCEe8eSTlZfhr1ZgyPZEui41gi0h2gsv3g",
-        authDomain: "testabcd-2962c.firebaseapp.com",
-        databaseURL: "https://testabcd-2962c.firebaseio.com",
-        projectId: "testabcd-2962c",
-        storageBucket: "testabcd-2962c.appspot.com",
-        messagingSenderId: "1087556312488",
-        appId: "1:1087556312488:web:a5816fbcb852700c"
-        */        
-        
         apiKey: "AIzaSyChCFT72Kw2kqRT5bOS0G_e3N6he_582I8",
         authDomain: "xcmg-38996.firebaseapp.com",
         databaseURL: "https://xcmg-38996.firebaseio.com",
